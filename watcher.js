906b928a9b53dc5c2833d1152f582c12e7875fdf
RC1140 <rc1140@republiccommandos.co.za>
2011-06-13 06:54:26 +0200

    Added settings file , added the web server and changed to delayed queuing

--- watcher.js
+++ watcher.js
@@ -1,16 +1,23 @@
 var stalker = require('stalker');
 var Notifo = require('notifo');
+var express = require('express');
+var settings = require('./settings');
+var app = express.createServer();
 
 var notification = new Notifo({
-    'username': ''
-    , 'secret': ''
+    'username': settings.notifo.username, 
+    'secret': settings.notifo.secret
 });
 
-stalker.watch('../snaps/', function (err, file) {
-	console.log('I saw a file. It was going like this: ' + file);
+stalker.watch(settings.monitorFolder, {buffer: 5000}, function(err, files) { 
+	var lastFileIndex = files.length - 1;
+        var filePath  = files[lastFileIndex].split('/');
+	var fileName = filePath[filePath.length - 1];
+	console.log('I saw a file. It was going like this: ' + fileName);
 	notification.send({
 	    'title': 'Front Door Activity',
-	    'msg': 'Motion Detected at the front door'
+	    'msg': 'Motion Detected at the front door',
+	    'uri': settings.serverURL + fileName
 	}, function(err, response) {
 	    if (err) {
 		console.log(err);
@@ -18,4 +25,11 @@ stalker.watch('../snaps/', function (err, file) {
 		console.log(response);
 	    }
 	});
+
 });
+
+app.get('/:snapName', function(req, res){
+    res.sendfile(settings.monitorFolder+req.params.snapName);
+});
+
+app.listen(3000);
